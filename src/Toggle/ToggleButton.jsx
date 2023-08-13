4994da2cc502136bba53b1107cec6196af87224b
Zouhair Sahtout <zouhairsahtout66@gmail.com>
2023-08-13 14:51:20 -0400

    replaced the button with a div for onClick

--- src/Toggle/ToggleButton.jsx
+++ src/Toggle/ToggleButton.jsx
@@ -7,11 +7,11 @@ function ToggleButton({ children }) {
 
 
     return (
-        <button 
+        <div 
             onClick={ toggle }
         >
             { children }
-        </button>
+        </div>
     );
 }
 
